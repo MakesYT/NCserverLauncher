7c97c4d4c94dcefcfad577d8e9af20d43e83ee3e
2696703792 <42534870+2696703792@users.noreply.github.com>
2020-09-11 17:01:30 +0800

    Initial commit

---
+++ src/main/java/top/ncserver/update/main.java
@@ -0,0 +1,12 @@
+package top.ncserver.update;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.FileNotFoundException;
+
+
+public class main {
+    public static void main( String[] args ) throws FileNotFoundException {
+       vsersion_check.check();
+    }
+}
---
+++ src/main/java/top/ncserver/update/vsersion_check.java
@@ -0,0 +1,18 @@
+package top.ncserver.update;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+import java.io.*;
+
+public class vsersion_check {
+    public static void check() throws FileNotFoundException {
+
+        File f = new File("a.txt");
+        FileOutputStream fop = new FileOutputStream(f);
+
+        FTP test = new FTP();
+        FTPClient ftp =FTP.getFTPClient("192.168.2.28",21,"update","n~7z26");
+        FTP.readFileByFolder(ftp, "/6周目/");
+
+    }
+}
