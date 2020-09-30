1c428ec09a84fefa7d139536fcf283c21138263c
2696703792 <42534870+2696703792@users.noreply.github.com>
2020-09-30 20:52:57 +0800

    UI施工中....

--- src/main/java/top/ncserver/update/main.java
+++ src/main/java/top/ncserver/update/main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class main {
     public static void main( String[] args ) throws IOException {
-       vsersion_check.check();
-       ui.Init_ui();
+       //vsersion_check.check();
+    ui.Init_ui();
     }
 }
--- src/main/java/top/ncserver/update/ui.java
+++ src/main/java/top/ncserver/update/ui.java
@@ -4,15 +4,26 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ui {
-    public static boolean Init_ui()
+    public static JFrame main = new JFrame("Ncharge更新器");
+    public static void Init_ui()
     {
-        JFrame main = new JFrame("Ncharge更新器");
+
         Point point = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
-        main.setSize((int) (point.x/1.5), (int) (point.x/1.5*0.618));
+        main.setSize((int) (point.x/1.75), (int) (point.x/1.75*0.618));
         main.setLocationRelativeTo(null);
         main.setResizable(false);
         main.setVisible(true);
+        ImageIcon icon = new ImageIcon("src/main/resources/server-icon.png");
+        JLabel icon1 = new JLabel();
+        JLabel title = new JLabel("Ncharge\n客户端更新程序");
+        title.setForeground(Color.black);
+        title.setBounds(150, 35,(int) (point.x/1.75) , 50);
+        Font font=new Font("宋体",Font.BOLD,30);
+        title.setFont(font);
+        icon1.setIcon(icon);
+        icon1.setBounds(1, 1, icon.getIconWidth(), icon.getIconHeight());
+        main.add(icon1);
+        main.add(title);
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        return false;
     }
 }
