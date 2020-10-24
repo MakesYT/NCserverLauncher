91062a6702f23bd9502a80317d65b6d1047a6dad
MakesYT <2696703792@qq.com>
2020-10-24 14:49:48 +0800

    初始化

--- src/main/java/top/ncserver/update/main.java
+++ src/main/java/top/ncserver/update/main.java
@@ -14,59 +14,7 @@ public class main {
     public static void main(String[] args) throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException, LaunchException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         alwaysOnTop.setAlwaysOnTop(true);
-        if (network_check()){
-            logger.info("网络连接正常");
-            if (ex_client.ex_check()) {
-                if (vsersion_check.check()) {
-                    logger.info("无需更新");
-                    JOptionPane.showMessageDialog(alwaysOnTop, "无需更新");
-                } else {
-                    if (!vsersion_check.client_version.substring(1, 2).equals(vsersion_check.server_version.substring(0, 1)))
-                    {
-                        JOptionPane.showMessageDialog(alwaysOnTop,"检测到当前客户端非最新周目，将进行周目更新");
-                        logger.info("检测到当前客户端非最新周目，将进行周目更新");
-                        File folder = new File(".minecraft");
-                        if (vsersion_check.deleteDir(folder))
-                        {
-                            JOptionPane.showMessageDialog(alwaysOnTop,"删除旧客户端完成");
-                            logger.info("删除旧客户端完成");
-                            ex_client.ex_check();
-                        }
 
-                        else {
-                            JOptionPane.showMessageDialog(alwaysOnTop, "删除旧客户端失败", "错误", JOptionPane.ERROR_MESSAGE);
-                            logger.error("删除旧客户端失败");
-                            System.exit(1);
-                        }
-
-                    }else
-                    update.start();
-                    if (vsersion_check.check())
-                        logger.info("更新完成。当前版本："+ vsersion_check.client_version);
-                    JOptionPane.showMessageDialog(alwaysOnTop, "更新完成!当前版本：" + vsersion_check.client_version);
-
-                }
-            }
-    }
-        else
-        {
-            JOptionPane.showMessageDialog(alwaysOnTop, "网络异常(甚至连接不上百度)", "错误", JOptionPane.ERROR_MESSAGE);
-            logger.error("网络异常(甚至连接不上百度)");
-        }
-        System.exit(0);
-    }
-
-    /**
-     */
-    static boolean network_check(){
-        logger.info("开始网络检测");
-        try {
-            InetAddress address = InetAddress.getByName("www.baidu.com");
-            return address.isReachable(1000);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 
 }
