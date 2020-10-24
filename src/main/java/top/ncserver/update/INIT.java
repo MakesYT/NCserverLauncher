c649a6feeb4d20fc5e7929fd333f6e732fc237c6
MakesYT <2696703792@qq.com>
2020-10-24 14:55:59 +0800

    初始化

--- src/main/java/top/ncserver/update/INIT.java
+++ src/main/java/top/ncserver/update/INIT.java
@@ -2,19 +2,16 @@ package top.ncserver.update;
 
 import org.apache.log4j.Logger;
 import org.to2mbn.jmccc.launch.LaunchException;
+
 import javax.swing.*;
-import java.io.File;
 import java.io.IOException;
-import java.net.InetAddress;
 
-
-public class main {
-    public static final Logger logger = Logger.getLogger(main.class);
+public class INIT {
+    public static final Logger logger = Logger.getLogger(INIT.class);
     public static JFrame alwaysOnTop =new JFrame("AlwaysOnTop");
     public static void main(String[] args) throws IOException, ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException, LaunchException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         alwaysOnTop.setAlwaysOnTop(true);
 
     }
-
 }
