80340f8df024507eeea4e39e86f7401852bc7b59
2696703792 <42534870+2696703792@users.noreply.github.com>
2021-01-08 21:13:25 +0800

    init

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -1,6 +1,39 @@
+/**
+ * @author MakesYT
+ */
+import com.alibaba.fastjson.JSON;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Config {
+    static Logger logger=Logger.getLogger(Config.class);
+
     public static void loader()
     {
+
+    }
+    static void init() {
+        Map<String, String> map=new LinkedHashMap<String, String>();
+        map.put("firstJoin","true");
+        map.put("update","false");
+        map.put("LauncherVersion","4.0.0-Alpha");
+        map.put("ClientVersion","null");
+        map.put("java","auto");
+        map.put("RAM","4096");
+        map.put("autoLogin","false");
+        map.put("account","null");
+        map.put("password","null");
+        try {
+            FileWriter config=new FileWriter("config.json");
+            config.write(JSON.toJSONString(map,true));
+            config.flush();
+            config.close();
+        }catch (Exception ignored){
+
+        }
 
     }
 }
--- src/main/java/INIT.java
+++ src/main/java/INIT.java
@@ -1,4 +1,10 @@
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * @author MakesYT
+ */
 public class INIT {
     static Logger logger=Logger.getLogger(INIT.class);
     public static void main(String[] args) {
